8d729e3d8f8ae9196a990ab0cb5f08126802b128
tigerking930 <vladislavcukanov@gmail.com>
2021-10-18 12:36:17 +0300

    laravel-vue-crud-sample-proejct on my own

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        return response()->json('You have successfully logged in', 200);
+    }
+
+    /**
+     * The user has logged out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function loggedOut(Request $request)
+    {
+        return response()->json('You have logged out now.', 200);
+    }
+}
---
+++ resources/js/routes.js
@@ -0,0 +1,21 @@
+import AllProduct from './components/AllProduct.vue';
+import CreateProduct from './components/CreateProduct.vue';
+import EditProduct from './components/EditProduct.vue';
+import Login from './components/Login.vue';
+
+export const routes = [{
+        name: 'home',
+        path: '/',
+        component: AllProduct
+    },
+    {
+        name: 'create',
+        path: '/create',
+        component: CreateProduct
+    },
+    {
+        name: 'edit',
+        path: '/edit/:id',
+        component: EditProduct
+    }
+];
